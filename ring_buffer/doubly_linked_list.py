4e48a4d3e58be8b62e7fd28d47fef9c95b2bc26a
Jojo Zhang <jojozhang0327@gmail.com>
2020-06-12 12:22:41 -0400

    finish ring buffer

---
+++ ring_buffer/doubly_linked_list.py
@@ -0,0 +1,175 @@
+"""Each ListNode holds a reference to its previous node
+as well as its next node in the List."""
+
+
+class ListNode:
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+    """Wrap the given value in a ListNode and insert it
+    after this node. Note that this node could already
+    have a next node it is point to."""
+
+    def insert_after(self, value):
+        current_next = self.next
+        self.next = ListNode(value, self, current_next)
+        if current_next:
+            current_next.prev = self.next
+
+    """Wrap the given value in a ListNode and insert it
+    before this node. Note that this node could already
+    have a previous node it is point to."""
+
+    def insert_before(self, value):
+        current_prev = self.prev
+        self.prev = ListNode(value, current_prev, self)
+        if current_prev:
+            current_prev.next = self.prev
+
+    """Rearranges this ListNode's previous and next pointers
+    accordingly, effectively deleting this ListNode."""
+
+    def delete(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
+
+
+"""Our doubly-linked list class. It holds references to
+the list's head and tail nodes."""
+
+
+class DoublyLinkedList:
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
+
+    def __len__(self):
+        return self.length
+
+    """Wraps the given value in a ListNode and inserts it
+    as the new head of the list. Don't forget to handle
+    the old head node's previous pointer accordingly."""
+
+    def add_to_head(self, value):
+        # create a new node
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        # check if the DLL is empty
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+    """Removes the List's current head node, making the
+    current head's next node the new head of the List.
+    Returns the value of the removed Node."""
+
+    def remove_from_head(self):
+        value = self.head.value
+        self.delete(self.head)
+        return value
+
+    """Wraps the given value in a ListNode and inserts it
+    as the new tail of the list. Don't forget to handle
+    the old tail node's next pointer accordingly."""
+
+    def add_to_tail(self, value):
+        # create a new Node
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        # check if the DLL is empty
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        # we have a non-empty list and add the new node to the tail
+        else:
+            # insert the new node after current tail
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            # point the tail to the new node
+            self.tail = new_node
+
+    """Removes the List's current tail node, making the
+    current tail's previous node the new tail of the List.
+    Returns the value of the removed Node."""
+
+    def remove_from_tail(self):
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
+
+    """Removes the input node from its current spot in the
+    List and inserts it as the new head node of the List."""
+
+    def move_to_front(self, node):
+        if node is self.head:
+            return
+        value = node.value
+        if node is self.tail:
+            self.remove_from_tail()
+        else:
+            node.delete()
+            self.length -= 1
+        self.add_to_head(value)
+
+    """Removes the input node from its current spot in the
+    List and inserts it as the new tail node of the List."""
+
+    def move_to_end(self, node):
+        if node is self.tail:
+            return
+        value = node.value
+        if node is self.head:
+            self.remove_from_head()
+            self.add_to_tail(value)
+        else:
+            node.delete()
+            self.length -= 1
+            self.add_to_tail(value)
+
+    """Removes a node from the list and handles cases where
+    the node was the head or the tail"""
+
+    def delete(self, node):
+        # if DLL is empty there's nothing to delete, we should return
+        if not self.head and not self.tail:
+            return
+        # decrement length of DLL
+        # would allow DLL to have a negative length
+        self.length -= 1
+        # if DLL has one element, remove it by setting head and tail pointers to None
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        # if node to delete is head, set DLL head pointer to node.next
+        elif self.head == node:
+            self.head = node.next
+            node.delete()
+        # if node to delete is tail, set DLL tail pointer to node.prev
+        elif self.tail == node:
+            self.tail = node.prev
+            node.delete()
+        # more than 3 nodes in our DLL, and not head or tail
+        else:
+            node.delete()
+
+    """Returns the highest value currently in the list"""
+
+    def get_max(self):
+        if not self.head:
+            return None
+        max_val = self.head.value
+        current = self.head
+        while current:
+            if current.value > max_val:
+                max_val = current.value
+            current = current.next
+        return max_val
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,33 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
+    def __init__(self, capacity=0):
+        self.capacity = capacity
+        self.current = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # before the list is full, keep adding item to the tail and track the most recent addition
+        if len(self.storage) < self.capacity:
+            self.current = self.storage.add_to_tail(item)
+        # when the list is full
+        elif len(self.storage) == self.capacity:
+            if self.current.next == None:
+                self.storage.remove_from_head()
+                self.current = self.storage.add_to_head(item)
+            else:
+                self.current.next.delete()
+                self.current = self.current.insert_after(item)
 
     def get(self):
-        pass
+        list_buffer_content = []
+        curr = self.storage.head
+
+        if curr is not None:
+            while curr.next is not None:
+                list_buffer_content.append(curr.value)
+                curr = curr.next
+            list_buffer_content.append(curr.value)
+
+        return list_buffer_content
